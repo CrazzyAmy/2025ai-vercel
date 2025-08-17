0cbc6b8381efb0e993d42a0da0ea44f3c14b2c44
unknown <amn82910@gmail.com>
2025-08-17 16:29:44 +0800

    [Fix] vercel.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      { source: '/forgot-password', destination: '/auth/forgot-password' },
+      { source: '/login', destination: '/auth/login' },
+      { source: '/signup', destination: '/auth/signup' },
+      { source: '/aigen', destination: '/auth/signup/page.tsx' },
+      { source: '/surveyedit', destination: '/auth/login/surveyedit/page.tsx' },
+      { source: '/surveyset', destination: '/auth/login/surveyset/page.tsx' },
+      // 視需要加其他 rewrites
+    ];
+  },
 };
 
 export default nextConfig;
